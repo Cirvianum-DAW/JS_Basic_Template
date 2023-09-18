afecb5b65a430b1e0bac0e0b425c8688401c003e
prosfp <110301233+prosfp@users.noreply.github.com>
2023-09-18 19:00:02 +0200

    Basic_Template

--- UF1/calculadora/calculadora.js
+++ UF1/calculadora/calculadora.js
@@ -11,23 +11,11 @@ const calculadora = {
   },
 
   // Funció per restar dos números
-  restar: (a, b) => {
-    return a - b;
-  },
 
   // Funció per multiplicar dos números
-  multiplicar: (a, b) => {
-    return a * b;
-  },
 
   // Funció per dividir dos números
-  dividir: (a, b) => {
-    // Verificar si b és 0 per evitar una divisió per zero
-    if (b === 0) {
-      throw new Error("No es pot dividir per zero.");
-    }
-    return a / b;
-  },
+  // Verificar si b és 0 per evitar una divisió per zero
 };
 
 // Exportar l'objecte calculator com a exportació per defecte
