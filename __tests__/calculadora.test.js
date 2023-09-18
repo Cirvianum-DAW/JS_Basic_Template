6edce59979f71c3b72551b371eb951cca4419f56
prosfp <110301233+prosfp@users.noreply.github.com>
2023-09-18 17:48:07 +0200

    fake_commit

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,7 +1,7 @@
 const calculadora = require("../UF1/calculadora/calculadora.js");
 
 test("sumar 2 + 3 és igual a 5", () => {
-  expect(calculadora.sumar(2, 3)).toBe(5);
+  expect(calculadora.sumar(2, 4)).toBe(5);
 });
 
 test("restar 5 - 3 és igual a 2", () => {
